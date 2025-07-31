aef97c6183c22a867ec0d7e9017a5e82b66e2bac
M Daffa A <daffa.alghifary62@gmail.com>
2025-07-31 10:24:11 +0700

    feat: create confirmation pop up

--- app/src/pages/home.jsx
+++ app/src/pages/home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import DeployForm from "../component/DeployFrom"
 import ContactList from "../component/ContractList"
+import ConfirmationPop from "../component/PopUp/ConfirmationPop"
 
 export default function Home() {
     const [escrows, setEscrows] = useState([])
@@ -20,7 +21,12 @@ export default function Home() {
 
     return(
         <div>
-            <h1 className='text-3xl mt-6 mb-10 text-white text-center font-bold font-mono'>Escrow Decentralized Application</h1>
+            <ConfirmationPop type="check box"/>
+            <div className="mt-6 mb-10">
+                <h1 className='text-3xl text-white text-center font-bold font-mono'>Escrow Decentralized Application</h1>
+                <p className="text-center text-white text-lg font-semibold">Project by <a href="https://github.com/alghifarydaffa62" target="_blank" className="text-blue-300">alghifarydaffa62</a></p>
+            </div>
+            
             <div className="flex justify-center gap-4">
                 <DeployForm onDeploy={handleNewEscrow}/>
                 <ContactList escrows={escrows}/>
